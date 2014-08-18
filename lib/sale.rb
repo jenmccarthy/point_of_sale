5dc504a02969426ea166a8369823e0df292dc82b
Randy Leighton & jennifermccarthy <student@epicodus.com>
2014-08-18 16:21:46 -0700

    associations between all classes are passing

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,3 +1,5 @@
 class Sale < ActiveRecord::Base
   has_many :purchases
+  belongs_to :customer
+  belongs_to :cashier
 end
--- spec/sale_spec.rb
+++ spec/sale_spec.rb
@@ -7,10 +7,19 @@ describe Sale do
     test_sale = Sale.create({customer_id: test_customer.id, cashier_id: test_cashier.id, date: '20140818'})
     expect(Sale.all).to eq [test_sale]
   end
+
   it 'has many purchases' do
     test_sale = Sale.create({customer_id: 1, cashier_id: 1, date: Time.now})
     test_purchase1 = Purchase.create({product_id: 123, quantity: 1, sale_id: test_sale.id})
     test_purchase2 = Purchase.create({product_id: 456, quantity: 1, sale_id: test_sale.id})
     expect(test_sale.purchases).to eq [test_purchase1, test_purchase2]
+  end
+
+  it 'belongs to a customer, belongs to a cashier' do
+    test_customer = Customer.create({name: 'Joseph'})
+    test_cashier = Cashier.create({name: 'Chachi'})
+    test_sale = Sale.create({customer_id: test_customer.id, cashier_id: test_cashier.id, date: Time.now})
+    expect(test_sale.customer).to eq test_customer
+    expect(test_sale.cashier).to eq test_cashier
   end
 end
