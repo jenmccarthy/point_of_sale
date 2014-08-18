89459845b65c98203a8345d2d8d706114abdf590
Randy Leighton & jennifermccarthy <student@epicodus.com>
2014-08-18 15:36:01 -0700

    associations for customer class have many sales and purchases through sales

--- spec/sale_spec.rb
+++ spec/sale_spec.rb
@@ -7,6 +7,4 @@ describe Sale do
     test_sale = Sale.create({customer_id: test_customer.id, cashier_id: test_cashier.id, date: '20140818'})
     expect(Sale.all).to eq [test_sale]
   end
-
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,13 @@
 require 'active_record'
+require 'rspec'
+require 'cashier'
+require 'sale'
+require 'purchase'
+require 'customer'
+require 'product'
+
+
+
 
 database_configurations = YAML::load(File.open('./db/config.yml'))
 test_configuration = database_configurations['test']
@@ -8,5 +17,9 @@ ActiveRecord::Base.establish_connection(test_configuration)
 RSpec.configure do |config|
   config.after(:each) do
     Cashier.all.each { |cashier| cashier.destroy }
+    Customer.all.each { |customer| customer.destroy }
+    Purchase.all.each { |purchase| purchase.destroy }
+    Sale.all.each { |sale| sale.destroy }
+    Product.all.each { |product| product.destroy}
   end
 end
