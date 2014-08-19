f3fb7c544304a57256421f2875df5b43f1df2489
Randy Leighton & jennifermccarthy <student@epicodus.com>
2014-08-19 16:15:35 -0700

    returns the number of each product purchased

--- lib/product.rb
+++ lib/product.rb
@@ -1,3 +1,13 @@
 class Product < ActiveRecord::Base
   has_many :purchases
+
+  def number_of_purchases
+    sum = 0
+    results = self.purchases.where(product_id: self.id)
+    results.each do |result|
+      sum += result.quantity
+    end
+    sum
+  end
+
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -9,4 +9,11 @@ describe Product do
     expect(test_product.purchases).to eq [test_purchase1, test_purchase2, test_purchase3]
   end
 
+  it 'will return the number of each product purchased' do
+    test_product = Product.create({name: 'Gerbil Dentures', price: 456.99})
+    test_purchase1 = Purchase.create({product_id: test_product.id, quantity: 1, sale_id: 37})
+    test_purchase2 = Purchase.create({product_id: test_product.id, quantity: 15, sale_id: 37})
+    test_purchase3 = Purchase.create({product_id: test_product.id, quantity: 16, sale_id: 39})
+    expect(test_product.number_of_purchases).to eq 32
+  end
 end
